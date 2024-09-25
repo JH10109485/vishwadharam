19e3eb6460bd9159e2260cd5f8d86b98f1557f65
ST10109485 <ST10109485@vcconnect.edu.za>
2024-09-25 08:46:32 +0200

    Redo

--- vishwadharam/vishwadharam/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace vishwadharam.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
